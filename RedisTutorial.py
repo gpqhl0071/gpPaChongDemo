be3048b536c9586ec3c20e375ac434bd84f56cb9
gao peng <1110@forex.com.cn>
2019-11-20 09:49:13 +0800

    add redis tutorial

---
+++ RedisTutorial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from splinter import Browser
+import re
+
+# open a browser
+browser = Browser('chrome')
+browser.visit('https://www.tutorialspoint.com/redis/redis_lists.htm')
+
+search_results_xpath = '/html/body/div[1]/div/div[1]/ul[2]/li'  # simple, right?
+
+search_results = browser.find_by_xpath(search_results_xpath)
+
+scraped_data = []
+for search_result in search_results:
+    title = search_result.html  # trust me
+    menuUrl = re.search('(?<=\")(.+?)(?=\")', title)
+    if menuUrl is not None:
+        newTitle1 = re.search('(?<=\>)(.+?)(?=\<)', title)
+
+        menuUrl = '(https://www.tutorialspoint.com/' + menuUrl.group() + ')'
+        menuTitle = '['+newTitle1.group()+']'
+
+        str = menuTitle + menuUrl
+
+        print(str)
+    # print(title)
